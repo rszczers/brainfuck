b072bfbb8c58cb2050db27b5a370260e03dddb13
rszczers <rafal.szczerski@gmail.com>
2019-03-21 15:56:38 +0100

    documentation

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,19 @@ import Evaluator
 import System.Environment
 import System.Exit
 
+-- | Brainfuck interpreter
+
 main :: IO ()
-main = getArgs >>=
-    opt >>=
+main = getArgs >>=  -- get inline arguments
+    opt >>=         -- print help or pass contents of source file to run
     (runProgram . parse)
 
-opt ["-h"] = usage >> exitWith ExitSuccess
+opt ["-h"] = usage >> exitWith ExitSuccess -- print help and exit
 opt []     = getContents -- allows to pass contents of file
                          -- in command line, like: bf < cat source.bf
-opt fs     = concat <$> mapM readFile fs
-
+opt fs     = concat <$> mapM readFile fs -- read multiple files 
+                                         -- (sources are concatenated
+                                         -- and evaluated as one 
+                                         -- program)
+-- | Prints help
 usage = putStrLn "Usage: bf [-h] [file ..]"
